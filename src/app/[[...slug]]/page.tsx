3b0e2eaceaa7bb9634a48256e1b1fdc36e06cc59
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2026-09-06 10:38:21 -0400

    main: no padding on small screens

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -10,7 +10,7 @@ export default function CatchAllPage() {
   return (
     <div className="flex min-h-screen flex-col bg-background text-foreground">
       <Header />
-      <main className="flex flex-1 flex-col items-center px-4 pb-28 pt-10 sm:px-0 md:pb-10">
+      <main className="flex flex-1 flex-col items-center sm:pt-10 md:pb-10">
         <TodoApp />
       </main>
     </div>
